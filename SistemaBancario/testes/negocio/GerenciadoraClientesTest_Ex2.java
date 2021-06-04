80eeb361bcba2c3c500c9041d8a5eca3e9bf7e53
Cristian <cristian.ataliba@outlook.com>
2021-06-04 19:38:23 -0300

    Atualiza curso testes automáticos de software - Udemy

--- SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex2.java
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex2.java
@@ -18,6 +18,7 @@ public class GerenciadoraClientesTest_Ex2 {
 		// criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 2, true);
+		Cliente cliente03 = new Cliente(3, "Ricardo Goncalves", 34, "ricardo.goncalves@outolook.com", 3, true);
 		
 		// inserindo os clientes criados na lista de clientes do banco
 		List<Cliente> clientesDoBanco = new ArrayList<>();
@@ -26,31 +27,42 @@ public class GerenciadoraClientesTest_Ex2 {
 		
 		gerClientes = new GerenciadoraClientes(clientesDoBanco);
 		
-		Cliente cliente = gerClientes.pesquisaCliente(1);
+		Cliente cliente = gerClientes.pesquisaCliente(2);
 		
-		assertThat(cliente.getId(), is(1));
+		assertThat(cliente.getId(), is(2));
+		assertThat(cliente.getEmail(), is("felipeaugusto@gmail.com"));
+		assertThat(cliente.getNome(), is("Felipe Augusto"));
 		
 	}
 	
+	
+	//Elaborado um teste para remover o cliente
 	@Test
 	public void testRemoveCliente() {
 
 		// criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 2, true);
+		Cliente cliente03 = new Cliente(3, "Ricardo Goancalves", 34, "ricardo.goncalves@outlook.com", 3, true);
+		
 		
 		// inserindo os clientes criados na lista de clientes do banco
 		List<Cliente> clientesDoBanco = new ArrayList<>();
 		clientesDoBanco.add(cliente01);
 		clientesDoBanco.add(cliente02);
+		clientesDoBanco.add(cliente03);
 		
-		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);// Instânciando a classe GerenciadoraCliente
+		//passando por parâmetro clientesDoBanco
 		
-		boolean clienteRemovido = gerClientes.removeCliente(2);
+		boolean clienteRemovido = gerClientes.removeCliente(2); //Chama o método remove cliente, passando com parãmetro o ID 2
 		
 		assertThat(clienteRemovido, is(true));
-		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
-		assertNull(gerClientes.pesquisaCliente(2));
+		
+		assertThat(gerClientes.getClientesDoBanco().size(), is(2));//Verifica que o tamanho da lista de clientes é 2, 
+		//pois um cliente foi removido.
+		
+		assertNull(gerClientes.pesquisaCliente(2)); // Pesquisa que o cliente removido está "vazio".
 		
 	}
 
