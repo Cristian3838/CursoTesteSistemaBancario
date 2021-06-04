0c8484a014ee61a47af0110431c84c0808a77402
Cristian <cristian.ataliba@outlook.com>
2021-06-04 18:49:53 -0300

    Atualiza curso de testes Udemy

--- SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex1.java
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest_Ex1.java
@@ -19,7 +19,7 @@ public class GerenciadoraClientesTest_Ex1 {
 		// criando alguns clientes
 		Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "gugafarias@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 2, true);
-		Cliente cliente03 = new Cliente(3, "Cristian Jonas Ataliba", 34, "cristian.ataliba@outlook.com",3, true);
+		Cliente cliente03 = new Cliente(3, "Ricardo Goncalves", 34, "ricardo.goncalves@outlook.com",3, true);
 		
 		//Após ter criado alguns clientes devemos criar uma lista para popular os clientes criados
 		
@@ -35,7 +35,7 @@ public class GerenciadoraClientesTest_Ex1 {
 		Cliente cliente = gerClientes.pesquisaCliente(3);// Chama o método pesquisa clientes passando o ID 1
 		
 		assertThat(cliente.getId(), is(3)); //assetthat significa verifique quem é  cliente 1
-		assertThat(cliente.getEmail(), is("cristian.ataliba@outlook.com"));
+		assertThat(cliente.getEmail(), is("ricardo.goncalves@outlook.com"));
 		assertThat(cliente.getIdade(), is(34));//Verifica o cliente com idade de 34 anos
 		
 	}
